6f291620a225ca9feb49399ac15f0ae990ab1bd4
RaynandCopia <145424545+RaynandCopia@users.noreply.github.com>
2025-04-08 15:46:28 +0800

    Add files via upload

---
+++ raynand-copia-main/services.php
@@ -0,0 +1,59 @@
+<!--services-->
+<section id="services">
+    <div class="container text-center">
+      <h2>SERVICES</h2 >
+        <div class="row row-cols-lg-3">
+          <div class="col d-flex justify-content-center">
+            <div class="card" style="width: 24rem;">
+              <img src="confusion.png" class="card-img-top mx-auto" alt="...">
+              <div class="card-body text-center">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col d-flex justify-content-center">
+            <div class="card" style="width: 24rem;">
+              <img src="confusion.png" class="card-img-top mx-auto" alt="...">
+              <div class="card-body text-center">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col d-flex justify-content-center">
+            <div class="card" style="width: 24rem;">
+              <img src="confusion.png" class="card-img-top mx-auto" alt="...">
+              <div class="card-body text-center">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col d-flex justify-content-center">
+            <div class="card" style="width: 24rem;">
+              <img src="confusion.png" class="card-img-top mx-auto" alt="...">
+              <div class="card-body text-center">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col d-flex justify-content-center">
+            <div class="card" style="width: 24rem;">
+              <img src="confusion.png" class="card-img-top mx-auto" alt="...">
+              <div class="card-body text-center">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          <div class="col d-flex justify-content-center">
+            <div class="card" style="width: 24rem;">
+              <img src="confusion.png" class="card-img-top mx-auto" alt="...">
+              <div class="card-body text-center">
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              </div>
+            </div>
+          </div>
+          
+        </div>
+       
+    </div>
+   <br><br>
+</section>
